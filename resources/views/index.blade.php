055492cae907760ed860c72da28dab7a4d3619e5
sari7happy <sari7happy@gmail.com>
2022-10-06 23:26:00 +0900

    ”laravel接続”

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    
+    <title>paiza bbs</title>
+    <style>body {padding:10px;}</style>
+</head>
+<body>
+    <h1>paiza bbs</h1>
+</body>
+</html>
